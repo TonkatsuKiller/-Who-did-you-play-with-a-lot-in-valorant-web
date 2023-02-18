1d27d0cb9edd142b07a0538a29a82fc77f4b570a
sbarashi <cks040425@gmail.com>
2023-02-19 02:32:57 +0900

    beta

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 app.listen(8002);
 
 const axios = require('axios');
-logic = async(name, tag) => {
+logic = async(name, tag, refresh) => {
 
     //현재 조회중인 플레이어명
     const target = `${name}#${String(tag).toLowerCase()}`;
@@ -16,19 +16,22 @@ logic = async(name, tag) => {
     //puuid 가져오기 및 파싱
     const puuid = await axios.get(`https://dak.gg/valorant/_next/data/0UwWEikFYni1rYUhT_nKN/ko/profile/${name}-${tag}.json?name=${name}-${tag}`).then(res => res.data.pageProps.account.account.puuid);
 
-    //전적 새로고침 (dak.gg)
-    await axios.get(`https://val.dakgg.io/api/v1/rpc/account-sync/by-puuid/${puuid}`);
+    //새로고침 활성화 시 전적 새로고침 (dak.gg)
+    if (refresh) await axios.get(`https://val.dakgg.io/api/v1/rpc/account-sync/by-puuid/${puuid}`);
 
     //루프 돌리기 (모든 페이지 정보 긁어오기)
     let page = 1;
     let result = [];
+    let matchesLen = 0;
     while (true) {
 
         //인게임정보 목록 가져오기
         const matchData = await axios.get(`https://val.dakgg.io/api/v1/accounts/${puuid}/matches?page=${page}`).then(res => res.data.matches);
 
         //인게임정보 목록이 빈 페이지면 결과 출력
-        if (matchData === undefined || matchData.length <= 0) return result;
+        if (matchData === undefined || matchData.length <= 0) return { players: result, matchesLen  };
+
+        matchesLen += matchData.length;
 
         //게임 데이터 개별 출력
         for (let i = 0; i < matchData.length; i++) {
@@ -47,12 +50,12 @@ logic = async(name, tag) => {
                 
                 //만약 출력된 플레이어와 조회중인 플레이어가 다르면 진행
                 if (getPlayer != target) {
-                    const tmp = result.findIndex(x => x.user === getPlayer);
-                    if (tmp === -1) {
+                    const idx = result.findIndex(x => x.user === getPlayer);
+                    if (idx === -1) {
                         if (getPlayer != null) result.push({ user: getPlayer, count: 1 });
                     }
                     else {
-                        result[tmp].count++;
+                        result[idx].count++;
                     }
                 } 
             }
@@ -66,16 +69,19 @@ app.post('/api/search', async (req, res) => {
     let name = String(req.body.name).split('#')[0];
     let tag = String(req.body.name).split('#')[1];
 
+    const refresh = req.body.refresh;
+
     let result;
     try {
-        result = await logic(name, tag);
+        result = refresh === "true" ? await logic(name, tag, true) : await logic(name, tag, false);
     }
-    catch {
+    catch (e) {
+        console.log(e)
         return res.send("err");
     }
-    
+
     //한판 이하인 사람 제외
-    const filter = result.filter(x => x.count > 1);
+    const filter = result.players.filter(x => x.count > 1);
 
     //내림차순 정렬
     filter.sort((a,b) => {
@@ -88,7 +94,7 @@ app.post('/api/search', async (req, res) => {
     console.log(`post ip: ${reqIP.getClientIp(req)}`);
 
     //값 반환
-    res.send(filter);
+    res.send({ filter, matchesLen: result.matchesLen });
 });
 
 app.get('/', async (req, res) => {
